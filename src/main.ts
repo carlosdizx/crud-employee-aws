1e1223861b2989196a50fccadf5f345d98e66e6f
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-09-29 17:37:07 -0500

    :tada: create project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log(`http://localhost:${3000}`);
+};
+
+(async () => {
+  await bootstrap();
+})();
