fd03d91629a7f1622c4759c19022aad18992ff50
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-09-29 19:39:14 -0500

    :heavy_plus_sign: config main for validate

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import AppModule from './app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 const bootstrap = async () => {
   const env = process.env.NODE_ENV;
@@ -21,6 +22,12 @@ const bootstrap = async () => {
       break;
   }
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
   console.log(`http://localhost:${3000}`);
 };
