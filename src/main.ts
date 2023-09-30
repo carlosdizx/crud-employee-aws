2f861b70dea116d12cffc04a8e4214607342a0a9
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-09-29 19:07:52 -0500

    :fire: remove code unnecessary

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import * as dotenv from 'dotenv';
 
 const bootstrap = async () => {
   const env = process.env.NODE_ENV;
-  console.log('Ambiente', env);
   switch (env) {
     case undefined:
       break;
