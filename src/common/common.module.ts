5f9cd525cc026abc566cfa5161f5d4f6c89e2dc9
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-09-30 11:26:02 -0500

    :sparkles: implement adapter in common module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { S3Service } from './service/aws.s3.service';
+import DynamodbAdapter from './adapters/dynamodb.adapter';
 
 @Global()
 @Module({
-  providers: [S3Service],
-  exports: [S3Service],
+  providers: [S3Service, DynamodbAdapter],
+  exports: [S3Service, DynamodbAdapter],
 })
 export default class CommonModule {}
