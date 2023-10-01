bdc3bae7e911782ab8335cbeace6a0177e00ad17
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-10-01 11:12:38 -0500

    :sparkles: change config script

--- resources/init.sh
+++ resources/init.sh
@@ -1,3 +1,3 @@
 aws s3 mb s3://employees --region us-east-1 --endpoint-url http://localhost:4566
-aws dynamodb create-table --cli-input-json file://employees.json --endpoint-url http://localhost:4566
+aws dynamodb create-table --cli-input-json file://tables/employee.json --endpoint-url http://localhost:4566
 
