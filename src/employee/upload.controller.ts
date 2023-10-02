3b1d88e1c2f61e1c2d9a3eb947c88c2b30167536
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-10-01 20:25:16 -0500

    :sparkles: add validation for id

--- src/employee/upload.controller.ts
+++ src/employee/upload.controller.ts
@@ -2,6 +2,8 @@ import {
   BadRequestException,
   Controller,
   Get,
+  Param,
+  ParseUUIDPipe,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -10,23 +12,31 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { S3Service } from '../common/service/aws.s3.service';
 import { FileUpload } from '../common/interfaces/file-upload';
 import fileValidate from '../common/helpers/fileFilter.helper';
+import { EmployeeService } from './employee.service';
 
 @Controller('employee/file/upload')
 export class UploadController {
   private readonly bucketName: string = 'employees';
-  constructor(private readonly s3Service: S3Service) {}
-  @Post()
+  constructor(
+    private readonly s3Service: S3Service,
+    private readonly employeeService: EmployeeService,
+  ) {}
+  @Post(':id')
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileValidate,
     }),
   )
-  async uploadFile(@UploadedFile() file: FileUpload) {
+  async uploadFile(
+    @UploadedFile() file: FileUpload,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
     if (!file) throw new BadRequestException('File not found in request');
+    await this.employeeService.findEmployeeById(id);
     const [name, extension] = file.originalname.split('.');
     await this.s3Service.uploadFile({
       bucketName: this.bucketName,
-      key: `${name}.${extension}`,
+      key: `${id}/${name}.${extension}`,
       data: file.buffer,
     });
   }
